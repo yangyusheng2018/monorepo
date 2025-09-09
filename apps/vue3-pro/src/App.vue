cde71ddfb558aa1a982898c19e3802391b8308c7
yangyusheng <1611337277@qq.com>
2025-09-09 11:41:08 +0800

    创建仓库

---
+++ apps/vue3-pro/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MyButton from '@Mycomponents/MyButton/MyButton.vue';
+</script>
+
+<template>
+  <div class="common-button">
+    <h1>You did it!</h1>
+    <p>123</p>
+    <MyButton></MyButton>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ apps/vue3-pro/src/components/MyButton/MyButton.vue
@@ -0,0 +1,29 @@
+<!-- workspace/components/MyButton/MyButton.vue -->
+<template>
+  <button class="my-button">{{ labelName }}</button>
+</template>
+
+<script lang="ts">
+import {defineComponent } from 'vue';
+export default defineComponent({
+  name: 'MyButton',
+  props: {
+    labelName: {
+      type: String,
+      default: 'Button'
+    }
+  }
+})
+</script>
+
+<style scoped>
+.my-button {
+  background-color: rgb(228, 13, 13);
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+</style>
+
